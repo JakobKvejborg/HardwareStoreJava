48035c4e6199729c187ad7201833c59fd7014b27
Jonas Glintborg <10503086@ucn.dk>
2024-01-08 10:21:45 +0100

    TEST
    TEST

--- src/tui/JFrame.java
+++ src/tui/JFrame.java
@@ -52,41 +52,36 @@ public class JFrame extends javax.swing.JFrame {
 	public JFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
+
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Sale Menu", null, panel, null);
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		Barcode = new JTextField();
 		panel.add(Barcode);
 		Barcode.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		panel.add(btnNewButton);
-		
+
 		JTextPane textPane = new JTextPane();
 		panel.add(textPane);
-	
-		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_1, null);
-		
+
 		JLabel lblNewLabel = new JLabel("New label");
-		panel_1.add(lblNewLabel);
-		
+
 		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
-		
+		tabbedPane.addTab("Loan Menu", null, tabbedPane_1, null);
+
 		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.addTab("Lend Menu", null, tabbedPane_2, null);
-		
+
 		JTabbedPane tabbedPane_3 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.addTab("Jakob tab", null, tabbedPane_3, null);
 

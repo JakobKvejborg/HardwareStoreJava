d3ac8dc03f5d59969e8599bc1b0727a09c1f52ae
JakobKvejborgUcn <10513107@ucn.dk>
2024-01-08 10:00:06 +0100

    Jakob
    adds JFrame test

---
+++ src/tui/JFrame.java
@@ -0,0 +1,81 @@
+package tui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Desktop;
+
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import javax.swing.JTextPane;
+
+public class JFrame extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JTextField Barcode;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Sale Menu", null, panel, null);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		Barcode = new JTextField();
+		panel.add(Barcode);
+		Barcode.setColumns(10);
+		
+		JTextPane textPane = new JTextPane();
+		panel.add(textPane);
+	
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		panel_1.add(lblNewLabel);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
+	}
+
+}
